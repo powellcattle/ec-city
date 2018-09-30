50eea1a4af1b2d08a0387d2b6a778b61964996f1
powellcattle <43384789+powellcattle@users.noreply.github.com>
2018-09-30 11:39:24 -0500

    Loading HGAC E911 address data

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import ec_arcpy_util
 import ec_psql_util
+import ec_util
 import psycopg2
 import arcpy
 import logging
@@ -7,15 +8,18 @@ import sys
 
 SQL_DROP_ADDRESSES_911 = "DROP TABLE IF EXISTS address.address_911"
 SQL_CREATE_ADDRESSES_911 = "CREATE TABLE address.address_911(" \
-                           "address_911_id SERIAL4 NOT NULL," \
+                           "address_911_id SERIAL4 NOT NULL, " \
                            "add_number INT NOT NULL, " \
-                           "st_prefix CHARACTER(1) NULL," \
-                           "st_name VARCHAR(100) NOT NULL," \
-                           "st_type VARCHAR(10) NULL," \
-                           "add_unit VARCHAR(10) NULL," \
-                           "add_full VARCHAR(50) NOT NULL," \
-                           "CONSTRAINT unique_address_911_pkey PRIMARY KEY (address_911_id))"
-SQL_INSERT_ADDRESSES_911 = "INSERT INTO address.address_911(add_number, st_prefix, st_name, st_type, add_unit, add_full) VALUES (%s,%s,%s,%s,%s,%s)"
+                           "st_prefix CHARACTER(1) NULL, " \
+                           "st_name VARCHAR(100) NOT NULL, " \
+                           "st_type VARCHAR(10) NULL, " \
+                           "add_unit VARCHAR(10) NULL, " \
+                           "add_full VARCHAR(50) NOT NULL, " \
+                           "add_source VARCHAR(10) NOT NULL, " \
+                           "fuzzy CHARACTER(4) NOT NULL, " \
+                           "CONSTRAINT unique_address_911_pkey PRIMARY KEY (address_911_id), " \
+                           "CONSTRAINT address_911_name_idx UNIQUE (add_full))"
+SQL_INSERT_ADDRESSES_911 = "INSERT INTO address.address_911(add_number, st_prefix, st_name, st_type, add_unit, add_full, add_source, fuzzy) VALUES (%s,%s,%s,%s,%s,%s,%s,soundex(%s)) ON CONFLICT ON CONSTRAINT address_911_name_idx DO NOTHING"
 
 logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s",
                     filename="test.log",
@@ -41,16 +45,21 @@ try:
     arcpy.AcceptConnections(workspace, False)
     arcpy.DisconnectUser(workspace, "ALL")
 
+    add_source = "HGAC"
+
     rows = arcpy.SearchCursor("ec.sde.address911", fields="add_number; prefix; name; st_type; add_unit; add_full")
     for row in rows:
-        print(row.getValue("add_full"))
-        add_number = row.getValue("add_number")
-        prefix = row.getValue("prefix")
-        name = row.getValue("name")
-        st_type = row.getValue("st_type")
-        add_unit = row.getValue("add_unit")
-        add_full = row.getValue("add_full")
-        cur.execute(SQL_INSERT_ADDRESSES_911, (add_number, prefix, name, st_type, add_unit, add_full))
+        add_number = ec_util.to_pos_int_or_none(row.getValue("add_number"))
+        if add_number is None:
+            continue
+        prefix = ec_util.to_upper_or_none(row.getValue("prefix"))
+        name = ec_util.to_upper_or_none(row.getValue("name"))
+        if name is None:
+            continue
+        st_type = ec_util.to_upper_or_none(row.getValue("st_type"))
+        add_unit = ec_util.to_upper_or_none(row.getValue("add_unit"))
+        add_full = ec_util.to_upper_or_none(row.getValue("add_full"))
+        cur.execute(SQL_INSERT_ADDRESSES_911, (add_number, prefix, name, st_type, add_unit, add_full, add_source, name))
 
     # data_sets = arcpy.ListDatasets()
     # for ds in data_sets:
@@ -63,6 +72,8 @@ try:
 
 except psycopg2.Error as e:
     print(e)
+    logging.error(e)
+    con.rollback()
 
 except:
     logging.error(sys.exc_info()[1])
