1b36a92d03e55bfa3a41e3c9fb1dff0515954f30
powellcattle <43384789+powellcattle@users.noreply.github.com>
2019-01-21 15:20:19 -0600

    added openlocationcode to project

--- test.py
+++ test.py
@@ -4,20 +4,9 @@ import zipfile
 import os
 import logging
 import inspect
-
-import arcpy
-
-logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s",
-                    filename="parcel_updates.log",
-                    filemode="w",
-                    level=logging.ERROR,
-                    datefmt="%m/%d/%Y %I:%M:%S %p")
-
-arcpy.env.workspace = r"C:\Users\spowell\AppData\Roaming\ESRI\Desktop10.6\ArcCatalog\localhost.sde"
-
-datasets = arcpy.ListDatasets("Boundary", r"Feature")
-assert(datasets)
+import openlocationcode
 
 
-feature_classes = arcpy.ListFeatureClasses("county_parcel", "All", "ec.sde.Boundary")
-assert(feature_classes)
+
+code = openlocationcode.encode(29,-96)
+print(code)
